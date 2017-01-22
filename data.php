11629a89462894f73e745ef23f900bc7bd77aa05
Mahdi Bagheri <mb.programmer@yahoo.com>
2017-01-22 13:11:57 +0330

    pagination first try

--- data.php
+++ data.php
@@ -4,45 +4,58 @@ $req = json_decode($_POST['req']);
 
 $filters = $req->filters;
 $sorts = $req->sorts;
-$itemPerPage = $req->itemPerPage;
+$pagination = $req->pagination;
 
-$users = getUsers($filters, $sorts, $itemPerPage);
+
+$db = new PDO('sqlite:database.sqlite');
+$query = 'SELECT * FROM users';
+$query = processFilters($query, $filters);
+$query = processSorts($query, $sorts);
+
+$queryCount = str_replace('*', 'count(*)', $query);
+$result = $db->query($queryCount);
+$pagination->total = (integer)$result->fetchColumn();
+
+$query = $query . ' limit ' . $pagination->per_page;
+$query = $query . ' offset ' . $pagination->from;
+
+$result = $db->query($query, PDO::FETCH_ASSOC);
+$users = $result->fetchAll();
 
 header('Content-Type: application/json');
-echo json_encode($users);
+echo json_encode([
+    'data' => $users,
+    'pagination' => $pagination,
+]);
 
-function getUsers($filters, $sorts, $itemPerPage)
-{
-
-    $db = new PDO('sqlite:database.sqlite');
-
-    $query = 'SELECT * FROM users';
-
-    $query = processFilters($query, $filters);
-
-    $query = processSorts($query, $sorts);
-
-    $result = $db->query($query, PDO::FETCH_ASSOC);
-
-    $users = $result->fetchAll();
-
-    return $users;
-}
 
 function processFilters($query, $filters)
 {
-    foreach ($filters as $filter) {
-        switch ($filter->type) {
-            case 'string':
-                $query .= ' where ' . $filter->name . $filter->operator . "'" . $filter->oprand1 . "'";
-                break;
+    if (count($filters) > 0) {
+        foreach ($filters as $filter) {
+            switch ($filter->operator) {
+                case 'equal':
+                    $query .= ' where ' . $filter->name . '=' . "'" . $filter->oprand1 . "'";
+                    break;
+                case 'contain':
+                    $query .= ' where ' . $filter->name . ' LIKE' . " '%" . $filter->oprand1 . "%'";
+                    break;
+            }
         }
     }
-    return $query;
 
+    return $query;
 }
 
 function processSorts($query, $sorts)
 {
+    if (count($sorts) > 0) {
+        $sql = ' order by ';
+        foreach ($sorts as $sort) {
+            $query .= ' order by ' . $sort->name . ' ' . $sort->type;
+        }
+        $query .= $sql;
+    }
+
     return $query;
 }
--- gridview.js
+++ gridview.js
@@ -4,14 +4,32 @@ var Gridview = function (element, options) {
 
     // options
     var $containerElement = $(element) || undefined;
+    var $paginationContainerElement = $(options.paginationContainerElement) || undefined;
 
-    var itemPerPage = options.itemPerPage || 15;
+    $paginationContainerElement.on('click', 'a', function (e) {
+        var $this = $(this);
+        $paginationContainerElement.closest('li').removeClass('active');
+        $this.closest('li').addClass('active');
+        pagination.curent_page = parseInt($this.text());
+        pagination.from = ((pagination.curent_page - 1) * pagination.per_page) + 1
+        pagination.to = pagination.from + pagination.per_page;
+
+        send();
+    });
+
     var onload = options.onload || new Function();
     var dataSourceUrl = options.dataSourceUrl;
     var extraData = options.extraData || {};
 
     // variables
     var resultRows = [];
+    var pagination = {
+        total: undefined,
+        current_page: 1,
+        per_page: 15,
+        from: 1,
+        to: 15,
+    };
     var filters = [];
     var sorts = [];
 
@@ -41,6 +59,12 @@ var Gridview = function (element, options) {
             oprand2: oprand2,
         });
     };
+    var addFilterEqual = function (name, type, oprand1) {
+        addFilter(name, type, 'equal', oprand1, undefined)
+    };
+    var addFilterContain = function (name, type, oprand1) {
+        addFilter(name, type, 'contain', oprand1, undefined)
+    };
     var removeFilter = function (name) {
         var result = false;
         for (var index = 0; index > filters.length; index++) {
@@ -57,6 +81,29 @@ var Gridview = function (element, options) {
         return true;
     };
 
+    // pagination
+    var createPagination = function () {
+        if ($paginationContainerElement) {
+
+            var pageCount = pagination.total / pagination.per_page
+            if ((pagination.total % pagination.per_page) > 0) {
+                pageCount++;
+            }
+
+            var paginationHtml = '<ul class="pagination">';
+            for (var i = 1; i <= pageCount; i++) {
+                if (pagination.current_page == i) {
+                    paginationHtml += '<li class="active"><a href="javascript:void(0)">' + i + '</a></li>';
+                } else {
+                    paginationHtml += '<li><a href="javascript:void(0)">' + i + '</a></li>';
+                }
+            }
+            paginationHtml += '</ul>';
+
+            $paginationContainerElement.html(paginationHtml)
+        }
+    };
+
 
     // request
     var send = function () {
@@ -64,10 +111,17 @@ var Gridview = function (element, options) {
         params['req'] = JSON.stringify({
             filters: filters,
             sorts: sorts,
-            itemPerPage: itemPerPage
+            pagination: pagination,
         });
-        $.post(dataSourceUrl, params, function (data) {
-            resultRows = data;
+
+        $.post(dataSourceUrl, params, function (result) {
+            resultRows = result.data;
+            pagination = result.pagination;
+            console.log(pagination)
+            for (var i = 0; i < resultRows.length; i++) {
+                resultRows[i].row_number = pagination.from + i
+            }
+            createPagination();
             onload(resultRows);
         });
     };
@@ -76,7 +130,8 @@ var Gridview = function (element, options) {
     // public function
     this.addAscendingSort = addAscendingSort;
     this.addDescendingSort = addDescendingSort;
-    this.addFilter = addFilter;
+    this.addFilterEqual = addFilterEqual;
+    this.addFilterContain = addFilterContain;
     this.removeFilter = removeFilter;
     this.removeAllFilters = removeAllFilters;
     this.send = send;
